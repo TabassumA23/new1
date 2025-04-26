8015eb0cf15979500f99ef34dad2356850520acf
Ahmed Tabassum <a.tabassum@se22.qmul.ac.uk>
2025-04-26 15:12:17 +0100

    g

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,10 @@
 import App from './App.vue';
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
+
 import router from './router/index.ts';
 import { createPinia } from 'pinia';
 
-import routes from './router/index.ts'; // Ensure the correct path to the routes file
+
 
 const app = createApp(App);
 const pinia = createPinia();
